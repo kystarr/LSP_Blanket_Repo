55f189f0abe8c112447e82231b9f30bb8a608387
rhian <rhian@LAPTOP-GFG7072I>
2024-09-12 22:18:49 -0400

    Adding comments

--- lsp_fall_2024/src/org/howard/edu/lsp/assignment2/WordCounting.java
+++ lsp_fall_2024/src/org/howard/edu/lsp/assignment2/WordCounting.java
@@ -11,30 +11,31 @@ public class WordCounting {
 
     public static void main(String[] args) throws FileNotFoundException 
     {
-        String filename = "words.text"; 
-        try (Scanner inputfile = new Scanner(new File(filename)))
+        String filename = "words.txt"; 
+        try (Scanner inputfile = new Scanner(new File(filename)))      
         {
-            HashMap<String, Integer> wordcounter = new HashMap<>();
-            Pattern wordpattern = Pattern.compile("\\b[a-zA-Z]{4,}\\b");
+            HashMap<String, Integer> wordcounter = new HashMap<>();               // using HashMap to show the name: # of occurrences
+            Pattern wordpattern = Pattern.compile("\\b[a-zA-Z]{4,}\\b");        // words must have at least 4 letters
             
-            while (inputfile.hasNext()) 
+            while (inputfile.hasNext())                // going through each word in the file
             {
                 String word = inputfile.next();
-                word = word.toLowerCase(); 
-                Matcher matcher = wordpattern.matcher(word);
+                word = word.toLowerCase();                            // makes all words lower case, making sure the code is case-insesnsitive
+                Matcher matcher = wordpattern.matcher(word);         
                 
-                if (matcher.matches()) 
+                if (matcher.matches())                                   // checking to see if words have 4 or less letters
                 {
             
-                    wordcounter.put(word, wordcounter.getOrDefault(word, 0) + 1);
+                    wordcounter.put(word, wordcounter.getOrDefault(word, 0) + 1);    // adding counter
                 }
             }
             
        
-            for (Map.Entry<String, Integer> entry : wordcounter.entrySet()) 
+            for (Map.Entry<String, Integer> entry : wordcounter.entrySet())    // iterating through HashMap
             {
-                System.out.println(entry.getKey() + ": " + entry.getValue());
+                System.out.println(entry.getKey() + ": " + entry.getValue());    // prints out word: # of occurrences
             }
         } 
     }
 }
+
