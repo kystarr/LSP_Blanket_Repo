56ba9268b281d2e97e4ef778fc25689552aa7ed4
rhian <rhian@LAPTOP-GFG7072I>
2024-09-12 20:45:04 -0400

    Adding Word Counter code.

--- lsp_fall_2024/src/org/howard/edu/lsp/assignment2/WordCounting.java
+++ lsp_fall_2024/src/org/howard/edu/lsp/assignment2/WordCounting.java
@@ -1,20 +1,41 @@
 package org.howard.edu.lsp.assignment2;
 import java.io.File;
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.io.FileNotFoundException;
 
 
 public class WordCounting {
 
-	public static void main(String[] args) throws Exception
-	{
-		Scanner inputfile = new Scanner (new File ("words.txt"));
-		
-		while(inputfile.hasNextLine())
-		{
-			System.out.print(inputfile.nextLine() + " ");
-		}
-		System.out.println();
-
-	}
-
+    public static void main(String[] args) throws FileNotFoundException 
+    {
+        String filename = "words.txt"; 
+        try (Scanner inputfile = new Scanner(new File(filename)))
+        {
+            HashMap<String, Integer> wordcounter = new HashMap<>();
+            Pattern wordpattern = Pattern.compile("\\b[a-zA-Z]{4,}\\b");
+            
+            while (inputfile.hasNext()) 
+            {
+                String word = inputfile.next();
+                word = word.toLowerCase(); 
+                Matcher matcher = wordpattern.matcher(word);
+                
+                if (matcher.matches()) 
+                {
+            
+                    wordcounter.put(word, wordcounter.getOrDefault(word, 0) + 1);
+                }
+            }
+            
+       
+            for (Map.Entry<String, Integer> entry : wordcounter.entrySet()) 
+            {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
+        } 
+    }
 }
